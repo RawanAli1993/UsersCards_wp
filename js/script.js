d58f119ac04303188159fc3b3035bacb320da04e
rawan ali <rawan.08.ali@gmail.com>
2022-10-17 03:22:06 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Access-Control-Allow-: "https://github.com";
-
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
@@ -28,14 +26,18 @@ fetch("https://raw.githubusercontent.com/RawanAli1993/UsersCards_wp/main/users.j
       // const card1 = userCardTemplate.ATTRIBUTE_NODE(id, user.id)
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
+      const body1 = card.querySelector("[data-body1]")
       card.setAttribute( 'id', newId )
       newId = newId+1
       // console.log(card.id);
       header.textContent = user.name
-      body.textContent = user.email
+      body.textContent = user.phone
+      body1.textContent = user.email
       userCardContainer.append(card)
       // userCardContainer.append(modal)
-      return { name: user.name, email: user.email,  element: card }
+      return { name: user.name, email: user.email, phone: user.phone, 
+        address:user.address, department:user.department, position_name:user.position_name, hire_date:user.hire_date, element: card }
+      // ( name, email, department, "address, position_name, "hire_date, phone )
 
     })
   })
@@ -50,18 +52,29 @@ fetch("https://raw.githubusercontent.com/RawanAli1993/UsersCards_wp/main/users.j
   
   // ocument.getElementsByClassName("modal-body").textContent;
   function getID(event){
-    const pop_name =  document.querySelector(".modal-body");
-    const pop_email =  document.querySelector(".b1");
-    // const pop_username =  document.querySelector(".b2");
-    const pop_id=  document.querySelector(".b3");
+    const pop_name =  document.querySelector(".fullname");
+    const pop_email =  document.querySelector(".email");
+    const pop_phone =  document.querySelector(".phone");
+    const pop_address=  document.querySelector(".address");
+    const pop_department=  document.querySelector(".department");
+    const pop_positionname=  document.querySelector(".positionname");
+    const pop_hiredate=  document.querySelector(".hiredate");
+    
+
     var card_id = event.srcElement.id
     console.log(card_id);
     console.log(users[card_id]);
     pop_name.textContent = users[card_id].name;
-    pop_email.textContent = "EMAIL   " +users[card_id].email;
-    // pop_username.textContent = users[card_id].username;
+    
+    pop_email.textContent = "EMAIL :   " +users[card_id].email;
+    pop_phone.textContent = users[card_id].phone;
+    pop_address.textContent = users[card_id].address;
+    pop_department.textContent = users[card_id].department;
+    pop_positionname.textContent = users[card_id].position_name;
 
-    pop_id.textContent = users[card_id].id;
+    pop_hiredate.textContent = users[card_id].hire_date;
+
+    // pop_id.textContent = users[card_id].id;
     
 
   }
